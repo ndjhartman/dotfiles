5cb95c4bcb0578e545e81cb435e5c59bd0162dae
Nathan Hartman <nathan.hartman@windriver.com>
2020-04-01 15:33:13 -0400

    added script to link dotfiles

---
+++ apply.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOTFILES="\
+		.vim/vimrc \
+		.tmux/.tmux.conf \
+		.tmux/.tmux.conf.local \
+		.bash_profile \
+		"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+for FILE in $DOTFILES; do
+		ln -s -f "$DIR"/"$FILE" $HOME
+done 
